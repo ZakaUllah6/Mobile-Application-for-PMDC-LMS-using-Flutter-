68951b91dcf12f80920d30e1eb939c9d7f9cdfb7
ZakaUllahMomannD <44844487+ZakaUllahMomannD@users.noreply.github.com>
2023-04-08 16:41:16 -0700

    Add files via upload

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <modal_progress_hud_nsn/modal_progress_hud_nsn_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  ModalProgressHudNsnPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ModalProgressHudNsnPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+}
